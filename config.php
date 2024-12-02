796773842ea45c16fe3cc249cd92af29e7cf71c3
Lucas Oliveira <lucas.santana@basisti.com.br>
2024-12-02 18:00:43 -0300

    feat: trazendo alteracao do texto do FAQ para o tema

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    "module.FAQ" => [
+        'support-message' => 'Não encontrou o que procurava? Entre em contato com o suporte por meio do canal <a href="mailto:suporte.mapa@cultura.gov.br" style="color: #00a2f0; display: inline;">suporte.mapa@cultura.gov.br</a>.',
+    ]
+];
